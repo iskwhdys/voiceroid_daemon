53972fc67d257c1e4836dd5a4c7fc9f273847f3d
iskwhdys <iskwhdys@gmail.com>
2023-05-06 22:06:42 +0900

    Chrome用拡張機能追加　

---
+++ VoiceroidDaemon/ChromeExtensions/PostVoiceroid/background.js
@@ -0,0 +1,67 @@
+﻿chrome.action.onClicked.addListener((tab) => {
+    if (tab.url.includes('chrome://')) {
+        return;
+    }
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: attachDocument
+    });
+});
+
+function attachDocument() {
+
+    function bodyKeyDown_RemoveEvent(event) {
+        if (event.keyCode === 27) {
+            detachDocumentEvent();
+        }
+    }
+
+    function detachDocumentEvent() {
+        document.removeEventListener("click", clickElement);
+        document.removeEventListener("keydown", bodyKeyDown_RemoveEvent);
+
+        document.head.removeChild(hoverStyle);
+    }
+
+    function clickElement(event) {
+        var clickedElement = event.target;
+
+        detachDocumentEvent();
+
+        doPost(clickedElement.outerText);
+    }
+
+    function doPost(callText) {
+        console.log(callText);
+        var options = {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'text/plain; charset=utf-8'
+            },
+            body: callText
+        };
+
+        fetch('http://localhost:8080/play', options)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.body;
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                alert("Error:" + error);
+            });
+    }
+
+    let hoverStyle = document.createElement("style");
+    hoverStyle.innerHTML = "*:hover { outline: 2px solid red !important; }";
+
+    document.addEventListener("click", clickElement);
+    document.addEventListener("keydown", bodyKeyDown_RemoveEvent);
+
+    document.head.appendChild(hoverStyle);
+}
